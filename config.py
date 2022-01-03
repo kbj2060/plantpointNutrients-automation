81c80e26462717b9f102e210cc35992cd3a077d4
kbj2060 <kbj2060@naver.com>
2022-01-03 10:13:35 +0900

    [Bug Fix] Circular import fixed

--- config.py
+++ config.py
@@ -5,10 +5,7 @@ from models.SwitchModels import LED, Valve, WaterPump
 SECTION = 's1/d1'
 # ADDRESS = 'http://192.168.0.7:8000'
 ADDRESS = 'http://127.0.0.1:8000'
-AUTOMATION_SUBJECTS = ['nutrientsupply', 'spraytime', 'sprayterm', 'watersupply']
-AUTOMATION_MODELS = [SprayTerm, SprayTime, WaterSupply, NutrientSupply]
-SENSOR_MODELS = [Current, WaterLevel, DHT22]
-MACHINES_MODELS = [Valve, WaterPump, LED]
+
 UNITS = {
     'nutrientsupply': 'mL',
     'watersupply': 'L',
