20bf973be9d9f56bdf36bedea807213eecb445b9
kbj2060 <kbj2060@naver.com>
2022-01-03 10:40:58 +0900

    [Bug Fix] Circular import fixed

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-from models.AutomationModels import NutrientSupply, SprayTerm, SprayTime, WaterSupply
-from models.SensorModels import DHT22, Current, WaterLevel
-from models.SwitchModels import LED, Valve, WaterPump
-
 SECTION = 's1/d1'
 # ADDRESS = 'http://192.168.0.7:8000'
 ADDRESS = 'http://127.0.0.1:8000'
