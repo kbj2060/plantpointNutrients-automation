a91a27119dad44f92711ab8e29ab281355c46cf9
kbj2060 <kbj2060@naver.com>
2022-01-05 00:37:49 +0900

    [New Feature] remove Current

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 
 if __name__ == "__main__":
+    GPIO.cleanup()
     print(f"DATE: {datetime.now()}")
     print("양액 자동화 시스템 시작합니다.")
     automation_models = AutomationCollector().get()
